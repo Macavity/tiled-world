35094e301fb511b3aff2a3d5b1c6d1af8e0da7b9
Alexander Pape <a.pape@paneon.de>
2016-04-22 22:22:00 +0200

    Add Angular2 base

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
 var elixir = require('laravel-elixir');
+var elixirTypescript = require('elixir-typescript');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+
 
 elixir(function(mix) {
+    /**
+     * Sass
+     */
     mix.sass('app.scss');
+
+    /**
+     * Vendor libraries
+     */
+    mix.copy('node_modules/angular2', 'public/angular2');
+    mix.copy('node_modules/rxjs', 'public/rxjs');
+    mix.copy('node_modules/systemjs', 'public/systemjs');
+    mix.copy('node_modules/es6-promise', 'public/es6-promise');
+    mix.copy('node_modules/es6-shim', 'public/es6-shim');
+    mix.copy('node_modules/zone.js', 'public/zone.js');
+
+    /**
+     * Typescript
+     */
+    mix.typescript(
+        'app.js',
+        'public/',
+        '/**/*.ts', {
+            "target": "ES5",
+            "module": "system",
+            "moduleResolution": "node",
+            "sourceMap": true,
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "removeComments": false,
+            "noImplicitAny": false
+        }
+    );
+
+
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,29 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="/es6-shim/es6-shim.min.js"></script>
+        <script src="/systemjs/dist/system-polyfills.js"></script>
+        <script src="/angular2/es6/dev/src/testing/shims_for_IE.js"></script>
+
+        <script src="/angular2/bundles/angular2-polyfills.js"></script>
+        <script src="/systemjs/dist/system.src.js"></script>
+        <script src="/rxjs/bundles/Rx.js"></script>
+        <script src="/angular2/bundles/angular2.dev.js"></script>
+
+        <script>
+            System.config({
+                "defaultJSExtensions": true,
+                packages: {
+                  typescript: {
+                        format: 'register',
+                        defaultExtension: 'js'
+                    }
+                }
+            });
+
+            System.import('typescript/boot')
+                .then(null, console.error.bind(console));
+        </script>
 
         <style>
             html, body {
@@ -39,6 +61,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <my-app>Loading</my-app>
             </div>
         </div>
     </body>
