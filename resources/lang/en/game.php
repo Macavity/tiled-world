df39acb54c02398efff7fb9090f4147a71069d96
Alexander Pape <a.pape@paneon.de>
2016-04-29 22:55:59 +0200

    Add old language file data for english and german

---
+++ resources/lang/en/game.php
@@ -0,0 +1,122 @@
+<?php
+
+return [
+    // Overall Header
+    'RPG' => 'The LAX-RPG',
+    'Inventory' => 'Inventory',
+    'Shop' => 'Marketplace',
+    'Map' => 'Map',
+    'HQ' => 'Headquarters',
+    'Ranking' => 'Ranking',
+    'Wiki Tutorial' => 'Learn playing LPG',
+    'Create Chara' => 'Create LPG Character',
+
+    // Diverses
+    'lpg_auto_inn' => 'Automatic Healing',
+    'lpg_auto_inn_explanation' => 'Rest automatically when your HP drop below the set value in percent.<br>Valid values between 0 and 99.',
+
+    // Jobnames
+    'aco' => "Acolyte",
+    'arc' => "Archer",
+    'mag' => "Mage",
+    'swd' => "Swordman",
+    'thf' => "Thief",
+
+    // General Labels
+    'PLAYER' => "Player",
+    'LVL' => "Level",
+    'LEVEL' => "Level",
+    'LEVELUP' => "You have advanced a level",
+    'LOCATION' => "Location",
+
+    //'INFO' => "",
+
+    'HP' => "HP",
+    'SP' => "SP",
+    'EXP' => "Experience(Exp)",
+    'JEXP' => "Job Experience(JExp)",
+    'STR' => "Strength(Str)",
+    'CON' => "Constitution(Con)",
+    'INT' => "Intelligence(Int)",
+    'DEX' => "Dexterity(Dex)",
+    'AGI' => "Agility(Agi)",
+    'LUK' => "Luck(Luk)",
+    'ATK' => "Attack",
+    'DEF' => "Physical Defense",
+    'DAM' => "Physical Attack",
+    'MATK' => "Magical Attack",
+    'MDEF' => "Magical Defense",
+    'SPEED' => "Speed",
+    'STPOINTS' => "Available Status Points: ",
+    'SKPOINTS' => "Availabel Skill Points: ",
+    'CRIT' => "Chance of a Critical Hit",
+    'HIT' => "Hit Rate",
+    'FLEE' => "Flee Rate",
+    'CAST' => "Cast Rate",
+
+    'RACE' => "Race",
+    'SIZE' => "Size",
+    'DROPS' => "Item Drops",
+    'HENCHMEN' => "Escorts",
+    'MAPS' => "Maps",
+    'HELMS' => "Helm",
+    'FACE' => "Face",
+    'BREAST' => "Breast",
+    'GARMENT' => "Garment",
+    'SHOES' => "Shoes",
+    'SHIELDS' => "Shield",
+    'LH' => "Left Hand",
+    'RH' => "Right Hand",
+    'ACC' => "Accessoires",
+    'FACTION' => "First Action",
+    'BGROUND' => "Battleground",
+    'NEXTACTION' => "Next Action",
+
+    'TITLE_EQUIP' => "Ready for Battle!",
+    'TITLE_OTHERS' => "Ready for Battle!",
+    'TITLE_EQUIP_MVP' => "Are you ready for a BOSS Battle?",
+
+    'CHEATER_0' => 'Oh no -.- no cheating please...',
+    'CHEATER_1' => 'Wann lernst du es? Kein cheaten bitte...',
+    'CHEATER_2' => 'Ich verspüre das Bedürfnis dich beim nächsten Cheatversuch ein paar Level zurückzustufen XD',
+    'CHEATER_3' => 'Ich glaube da will jemand nochmal bei Level 1 anfangen *gg*',
+    'CHEATER_4' => 'So? du meldest dich freiwillig für den Putzdienst? OK ^.^',
+
+    // RPG Portal
+    'GENERAL' => 'General',
+    'ACTIVE_CHARA' => 'Active Character',
+    'RPG_SUPPORT_FORUM' => 'Support - Forum',
+    'RPG_SUPPORT_WIKI' => 'Support - RPG Wiki',
+    'TOOLS' => 'Tools',
+
+    'PORTAL' => 'Last Anixile Portal',
+    'FORUM' => 'Last Anixile Forum',
+    'CHARPAGE' => 'Your Character Page',
+    'SKILLS' => 'Your Skills',
+    'EQUIPMENT' => 'Your Equipment',
+    'INVENTORY' => 'Your Inventory',
+    'CHARAS' => 'Your Characters',
+    'MAP' => 'Map',
+    'CAULDRON' => 'Cauldron',
+    'INN' => 'Inn/ Rest',
+    'EVENTSHOP' => 'Event Shop',
+    'MARKETPLACE' => 'Marketplace',
+    'QUEST' => 'Quests',
+    'JOBQUEST' => 'Job Quests',
+    'RANKING' => 'Ranking List',
+    'FIGHTS' => 'Your Fights',
+
+    'RPG_FORUM' => 'RPG Forum',
+    'RPG_QUESTIONS' => 'Questions Thread',
+    'RPG_BUGS' => 'Bugs Thread',
+    'RPG_TECH' => 'Tech Forum',
+
+    'RPG_WIKI' => 'RPG Wiki',
+    'RPG_TUTORIAL' => 'Tutorial',
+    'RPG_MAP' => 'Map Overview',
+
+    'CALENDAR' => 'Calendar',
+    'ALBUM' => 'Album',
+    'LINKS' => 'Links',
+
+];
